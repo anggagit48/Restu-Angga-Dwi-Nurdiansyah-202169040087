f62d7ac5da0aed77c6964e1da82109cbcfb03e5f
Restu Angga <gitretsu48@gmail.com>
2023-07-07 16:11:32 +0700

    membuat model

---
+++ app/Models/barang.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class barang extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'Nama',
+        'Jumlah',
+        'Harga Satuan'
+    ];
+}
